d93757560e3b837f3bca5a7e389f66a22faf796a
lexiealbert78 <la1168@york.ac.uk>
2024-04-26 10:50:35 +0100

    Added activities

--- assets/map/Football.tsx
+++ assets/map/Football.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Football" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../../../Football.png" width="35" height="34"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Football" tilewidth="15" tileheight="15" tilecount="1" columns="1">
+ <image source="../../../../Football.png" width="15" height="15"/>
 </tileset>
