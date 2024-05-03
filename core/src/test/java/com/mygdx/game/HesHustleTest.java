28ba1405378c639e54bb27ade7797adc0b41b893
maxnoglik <mn1102@york.ac.uk>
2024-05-03 15:25:58 +0100

    Tests here we go again

---
+++ core/src/test/java/com/mygdx/game/HesHustleTest.java
@@ -0,0 +1,44 @@
+package com.mygdx.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HesHustleTest {
+
+    @Test
+    void dispose() {
+    }
+
+    @Test
+    void pause() {
+    }
+
+    @Test
+    void resume() {
+    }
+
+    @Test
+    void render() {
+    }
+
+    @Test
+    void resize() {
+    }
+
+    @Test
+    void setScreen() {
+    }
+
+    @Test
+    void getScreen() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void testRender() {
+    }
+}
---
+++ core/src/test/java/com/mygdx/game/screens/UsernameManagerTest.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.screens;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsernameManagerTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+}
