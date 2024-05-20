696b603d89c5c888e67a738102496d50ed009e7b
Max-0000777 <160613373+Max-0000777@users.noreply.github.com>
2024-05-20 15:46:19 +0100

    Add files via upload

---
+++ tests/src/com/mygdx/game/AssetTests.java
@@ -0,0 +1,461 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(GdxTestRunner.class)
+public class AssetTests {
+
+    @Test
+    public void testBackgroundAssetExists() {
+        assertTrue(Gdx.files.internal("background.png").exists());
+
+    }
+    @Test
+    public void testBenchWoodAssetExists() {
+        assertTrue(Gdx.files.internal("bench-wood.png").exists());
+
+    }
+    @Test
+    public void testCharacterAssetExists() {
+        assertTrue(Gdx.files.internal("Character.png").exists());
+
+    }
+    @Test
+    public void testContinueButtonAssetExists() {
+        assertTrue(Gdx.files.internal("ContinueButton.png").exists());
+
+    }
+    @Test
+    public void testEndScreenBackgroundAssetExists() {
+        assertTrue(Gdx.files.internal("endScreenBackground.png").exists());
+
+    }
+    @Test
+    public void testEnergyBarAssetExists() {
+        assertTrue(Gdx.files.internal("energy_bar.png").exists());
+
+    }
+    @Test
+    public void testEnergyBarTempAssetExists() {
+        assertTrue(Gdx.files.internal("energy_bar_temp.png").exists());
+
+    }
+    @Test
+    public void testEnergyFillGreenAssetExists() {
+        assertTrue(Gdx.files.internal("energy_fill_green.png").exists());
+
+    }
+    @Test
+    public void testEnergyFillRedAssetExists() {
+        assertTrue(Gdx.files.internal("energy_fill_red.png").exists());
+
+    }
+    @Test
+    public void testEnergyFillYellowAssetExists() {
+        assertTrue(Gdx.files.internal("energy_fill_yellow.png").exists());
+
+    }
+    @Test
+    public void testFootballAssetExists() {
+        assertTrue(Gdx.files.internal("Football.png").exists());
+
+    }
+    @Test
+    public void testFullScreenButtonAssetExists() {
+        assertTrue(Gdx.files.internal("FullScreenButton.png").exists());
+
+    }
+    @Test
+    public void testGoalAssetExists() {
+        assertTrue(Gdx.files.internal("goal.png").exists());
+
+    }
+    @Test
+    public void testInteractionPopupsAssetExists() {
+        assertTrue(Gdx.files.internal("InteractionPopups.png").exists());
+
+    }
+    @Test
+    public void testLeaderButtonAssetExists() {
+        assertTrue(Gdx.files.internal("LeaderButton.png").exists());
+
+    }
+    @Test
+    public void testLogoAssetExists() {
+        assertTrue(Gdx.files.internal("Logo.png").exists());
+
+    }
+    @Test
+    public void testMarkerPixelAssetExists() {
+        assertTrue(Gdx.files.internal("marker_pixel.png").exists());
+
+    }
+    @Test
+    public void testMarkerAssetExists() {
+        assertTrue(Gdx.files.internal("Markers.png").exists());
+
+    }
+
+    @Test
+    public void testMinimiseButtonAssetExists() {
+        assertTrue(Gdx.files.internal("MinimiseButton.png").exists());
+
+    }
+    @Test
+    public void testMutedAssetExists() {
+        assertTrue(Gdx.files.internal("Muted.png").exists());
+
+    }
+    @Test
+    public void testQuitButtonAssetExists() {
+        assertTrue(Gdx.files.internal("QuitButton.png").exists());
+
+    }
+    @Test
+    public void testResumeButtonAssetExists() {
+        assertTrue(Gdx.files.internal("ResumeButton.png").exists());
+
+    }
+    @Test
+    public void testScoresAssetExists() {
+        assertTrue(Gdx.files.internal("scores.txt").exists());
+
+    }
+    @Test
+    public void testSignAssetExists() {
+        assertTrue(Gdx.files.internal("sign.png").exists());
+
+    }
+    @Test
+    public void testSpriteSheetAssetExists() {
+        assertTrue(Gdx.files.internal("SpriteSheet.png").exists());
+
+    }
+    @Test
+    public void testStartButtonAssetExists() {
+        assertTrue(Gdx.files.internal("StartButton.png").exists());
+
+    }
+    @Test
+    public void testTitleAssetExists() {
+        assertTrue(Gdx.files.internal("Title.png").exists());
+
+    }
+    @Test
+    public void testVolumeButtonAssetExists() {
+        assertTrue(Gdx.files.internal("VolumeButton.png").exists());
+
+    }
+    @Test
+    public void testWhiteAssetExists() {
+        assertTrue(Gdx.files.internal("white.png").exists());
+
+    }
+    @Test
+    public void testMapOutsideAssetExists() {
+        assertTrue(Gdx.files.internal("map/outside/outside.png").exists());
+
+    }
+    @Test
+    public void testMapTrees055AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile055.png").exists());
+
+    }
+    @Test
+    public void testMapTrees056AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile056.png").exists());
+
+    }
+    @Test
+    public void testMapTrees057AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile057.png").exists());
+
+    }
+    @Test
+    public void testMapTrees058AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile058.png").exists());
+
+    }
+    @Test
+    public void testMapTrees059AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile059.png").exists());
+
+    }
+    @Test
+    public void testMapTrees060AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile060.png").exists());
+
+    }
+    @Test
+    public void testMapTrees068AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile068.png").exists());
+
+    }
+    @Test
+    public void testMapTrees069AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile069.png").exists());
+
+    }
+    @Test
+    public void testMapTrees070AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile070.png").exists());
+
+    }
+    @Test
+    public void testMapTrees071AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile071.png").exists());
+
+    }
+    @Test
+    public void testMapTrees072AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile072.png").exists());
+
+    }
+    @Test
+    public void testMapTrees073AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile073.png").exists());
+
+    }
+    @Test
+    public void testMapTrees081AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile081.png").exists());
+
+    }
+    @Test
+    public void testMapTrees082AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile082.png").exists());
+
+    }
+    @Test
+    public void testMapTrees083AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile083.png").exists());
+
+    }
+    @Test
+    public void testMapTrees084AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile084.png").exists());
+
+    }
+    @Test
+    public void testMapTrees085AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile085.png").exists());
+
+    }
+    @Test
+    public void testMapTrees086AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile086.png").exists());
+
+    }
+    @Test
+    public void testMapTrees094AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile094.png").exists());
+
+    }
+    @Test
+    public void testMapTrees095AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile095.png").exists());
+
+    }
+    @Test
+    public void testMapTrees096AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile096.png").exists());
+
+    }
+    @Test
+    public void testMapTrees097AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile097.png").exists());
+
+    }
+    @Test
+    public void testMapTrees098AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile098.png").exists());
+
+    }
+    @Test
+    public void testMapTrees099AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile099.png").exists());
+
+    }
+    @Test
+    public void testMapTrees107AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile107.png").exists());
+
+    }
+    @Test
+    public void testMapTrees108AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile108.png").exists());
+
+    }
+    @Test
+    public void testMapTrees109AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile109.png").exists());
+
+    }
+    @Test
+    public void testMapTrees110AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile110.png").exists());
+
+    }
+    @Test
+    public void testMapTrees111AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile111.png").exists());
+
+    }
+    @Test
+    public void testMapTrees112AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile112.png").exists());
+
+    }
+    @Test
+    public void testMapTrees599AssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees/tile599.png").exists());
+
+    }
+    @Test
+    public void testMapBenchWoodAssetExists() {
+        assertTrue(Gdx.files.internal("map/bench-wood.tsx").exists());
+
+    }
+    @Test
+    public void testMapBuildingCompSciAssetExists() {
+        assertTrue(Gdx.files.internal("map/Building-CompSci.png").exists());
+
+    }
+    @Test
+    public void testMapBuildingCompSci2AssetExists() {
+        assertTrue(Gdx.files.internal("map/Building-CompSci.tsx").exists());
+
+    }
+    @Test
+    public void testMapBuildingLangwithAssetExists() {
+        assertTrue(Gdx.files.internal("map/Building-Langwith.png").exists());
+
+    }
+    @Test
+    public void testMapBuildingLangwith2AssetExists() {
+        assertTrue(Gdx.files.internal("map/Building-Langwith.tsx").exists());
+
+    }
+    @Test
+    public void testMapBuildingPiazzaAssetExists() {
+        assertTrue(Gdx.files.internal("map/Building-Piazza.png").exists());
+
+    }
+    @Test
+    public void testMapBuildingPiazza2AssetExists() {
+        assertTrue(Gdx.files.internal("map/Building-Piazza.tsx").exists());
+
+    }
+    @Test
+    public void testMapBuildingRonCookeHubAssetExists() {
+        assertTrue(Gdx.files.internal("map/Building-RonCookeHub.png").exists());
+
+    }
+    @Test
+    public void testMapBuildingRonCookeHub2AssetExists() {
+        assertTrue(Gdx.files.internal("map/Building-RonCookeHub.tsx").exists());
+
+    }
+    @Test
+    public void testMapFenceAssetExists() {
+        assertTrue(Gdx.files.internal("map/fence.png").exists());
+
+    }
+    @Test
+    public void testMapFence2AssetExists() {
+        assertTrue(Gdx.files.internal("map/Fence.tsx").exists());
+
+    }
+    @Test
+    public void testMapFootballAssetExists() {
+        assertTrue(Gdx.files.internal("map/Football.tsx").exists());
+
+    }
+    @Test
+    public void testMapOutside2AssetExists() {
+        assertTrue(Gdx.files.internal("map/outside.tsx").exists());
+
+    }
+    @Test
+    public void testMapTreesAssetExists() {
+        assertTrue(Gdx.files.internal("map/Trees.tsx").exists());
+
+    }
+    @Test
+    public void testMapWaterFowlAssetExists() {
+        assertTrue(Gdx.files.internal("map/waterfowl.png").exists());
+
+    }
+    @Test
+    public void testMapWaterFowl2AssetExists() {
+        assertTrue(Gdx.files.internal("map/waterfowl.tsx").exists());
+
+    }
+    @Test
+    public void testMetaManifestAssetExists() {
+        assertTrue(Gdx.files.internal("META-INF/MANIFEST.MF").exists());
+
+    }
+    @Test
+    public void testSfxBgMusicAssetExists() {
+        assertTrue(Gdx.files.internal("sfx/bg_music.wav").exists());
+
+    }
+    @Test
+    public void testSfxDucksAssetExists() {
+        assertTrue(Gdx.files.internal("sfx/ducks.mp3").exists());
+
+    }
+    @Test
+    public void testSfxEatingAssetExists() {
+        assertTrue(Gdx.files.internal("sfx/eating.mp3").exists());
+
+    }
+    @Test
+    public void testSfxNoEnergyAssetExists() {
+        assertTrue(Gdx.files.internal("sfx/no_energy.mp3").exists());
+
+    }
+    @Test
+    public void testSfxNoEnergyLongAssetExists() {
+        assertTrue(Gdx.files.internal("sfx/no_energy_long.mp3").exists());
+
+    }
+    @Test
+    public void testSfxSleepingFadeOutAssetExists() {
+        assertTrue(Gdx.files.internal("sfx/sleeping_fade_out.mp3").exists());
+
+    }
+    @Test
+    public void testSfxStudyingAssetExists() {
+        assertTrue(Gdx.files.internal("sfx/studying.mp3").exists());
+
+    }
+    @Test
+    public void testSkinFontExportAssetExists() {
+        assertTrue(Gdx.files.internal("skin/font-export.fnt").exists());
+
+    }
+    @Test
+    public void testSkinGlassyUiAssetExists() {
+        assertTrue(Gdx.files.internal("skin/glassy-ui.atlas").exists());
+
+    }
+    @Test
+    public void testSkinGlassyUi2AssetExists() {
+        assertTrue(Gdx.files.internal("skin/glassy-ui.json").exists());
+
+    }
+    @Test
+    public void testSkinGlassyUi3AssetExists() {
+        assertTrue(Gdx.files.internal("skin/glassy-ui.png").exists());
+
+    }
+
+
+}
